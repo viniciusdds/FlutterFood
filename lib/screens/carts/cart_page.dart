36b63cbac73e74bcda0434dc7cb0549370a8ef95
viniciusdds <viniciusdamasceno180@hotmail.com>
2020-08-22 15:56:39 -0300

    Aula 21 finalizada

--- lib/screens/carts/cart_page.dart
+++ lib/screens/carts/cart_page.dart
@@ -18,12 +18,14 @@ class CartScreen extends StatelessWidget {
   }
 
   Widget _buildContentCart(context){
-    return Column(
+    return ListView(
+      shrinkWrap: false,
       children: <Widget>[
         _buildHeader(),
         _buildCartList(context),
-        //_buildFormComment(),
-        //_buildCheckout(),
+        _buildTextTotalCart(),
+        _buildFormComment(context),
+        _buildCheckout(),
       ],
     );
   }
@@ -37,33 +39,53 @@ class CartScreen extends StatelessWidget {
   }
 
   Widget _buildCartList(context){
-    return Container(
-      height: (MediaQuery.of(context).size.height - 180),
-      child: ListView.builder(
-        itemCount: 6,
-        itemBuilder: (context, index) => _buildCartItem(context),
-      ),
+    return ListView.builder(
+      shrinkWrap: true,
+      primary: false,
+      itemCount: 6,
+      itemBuilder: (context, index) => _buildCartItem(context),
     );
   }
 
   Widget _buildCartItem(context){
-    return Container(
-      margin: EdgeInsets.all(10),
-      height: 80,
-      decoration: BoxDecoration(
-        color: Colors.grey.shade100,
-        border: Border.all(color: Colors.grey[200]),
-        borderRadius: BorderRadius.all(Radius.circular(16))
-      ),
-      child: Container(
-        padding: EdgeInsets.all(2),
-        child: Row(
-          children: <Widget>[
-            ShowImageCachedNetwork('https://eurio.com.br/hf-conteudo/templates/eurio2/img/sem_foto_icone.jpg'),
-            _showDetailItemCart(context),
-          ],
+    return Stack(
+      children: [
+        Container(
+        margin: EdgeInsets.all(10),
+        height: 80,
+        decoration: BoxDecoration(
+          color: Colors.grey.shade100,
+          border: Border.all(color: Colors.grey[200]),
+          borderRadius: BorderRadius.all(Radius.circular(16))
+        ),
+        child: Container(
+          padding: EdgeInsets.all(2),
+          child: Row(
+            children: <Widget>[
+              ShowImageCachedNetwork('https://eurio.com.br/hf-conteudo/templates/eurio2/img/sem_foto_icone.jpg'),
+              _showDetailItemCart(context),
+            ],
+          ),
         ),
       ),
+        Align(
+          alignment: Alignment.topRight,
+          child: Container(
+            height: 25,
+            width: 25,
+            margin: EdgeInsets.only(top: 2, right: 4),
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.all(Radius.circular(100))
+            ),
+            child: Icon(
+              Icons.close,
+              size: 20,
+              color: Colors.white
+            ),
+          ),
+        )
+      ],
     );
   }
 
@@ -107,6 +129,66 @@ class CartScreen extends StatelessWidget {
             )
           ],
         ),
+      ),
+    );
+  }
+
+  Widget _buildTextTotalCart(){
+    return Container(
+      margin: EdgeInsets.only(left: 10, right: 10, top: 26, bottom: 16),
+      child: Text("Preço total: R\$ 499,00",
+        style: TextStyle(color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
+      ),
+    );
+  }
+
+  Widget _buildFormComment(context){
+    return Container(
+      padding: EdgeInsets.all(10),
+      child: TextFormField(
+        autocorrect: true,
+        style: TextStyle(color: Theme.of(context).primaryColor),
+        cursorColor:  Theme.of(context).primaryColor,
+        onSaved: (value){
+          print(value);
+        },
+        decoration: InputDecoration(
+          labelText: 'Comentário (ex: sem cebola)',
+          labelStyle: TextStyle(color:  Theme.of(context).primaryColor),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(16),
+            borderSide: BorderSide(color:  Theme.of(context).primaryColor)
+          ),
+          enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(16),
+              borderSide: BorderSide(color:  Theme.of(context).primaryColor),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCheckout(){
+    return Container(
+      margin: EdgeInsets.only(top: 10, bottom: 50, right: 10, left: 10),
+      decoration: BoxDecoration(
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey,
+            offset: Offset(0.0, 1.0),
+            blurRadius: 10
+          )
+        ],
+        borderRadius: BorderRadius.all(Radius.circular(16)),
+        color: Colors.orange[800]
+      ),
+      child: RaisedButton(
+          onPressed: (){
+            print('checkout');
+          },
+        child: Text('Finalizar Pedido'),
+        color: Colors.transparent,
+        elevation: 0,
       ),
     );
   }
